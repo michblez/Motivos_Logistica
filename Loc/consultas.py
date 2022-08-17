5d54f6405b963dc3f11a20fdb1a45e99c94a8a4c
Mich <84158543+michblez@users.noreply.github.com>
2022-08-17 20:30:39 -0300

    Fix:

--- Loc/consultas.py
+++ Loc/consultas.py
@@ -1,3 +1,4 @@
+#Você deve importar as seguintes bibliotecas para funcionar o codigo!
 from sqlalchemy import text 
 from controller import get_connection
 from datetime import date, datetime
@@ -16,7 +17,7 @@ class motivo():
 
   def __init__(self, planilha) -> None:
     self.planilha = self.planilha_excel(planilha)
-  
+  #recebe planilha e trata colunas
   def planilha_excel(self, planilha):
 
     df = pd.read_excel(planilha)
@@ -46,57 +47,7 @@ Quantidade_Entregue_Errada = df_motivo.contagem_filtramotivo('Item entregue na q
 Item_Entregue_Errado = df_motivo.contagem_filtramotivo('Item entregue errado')
 Ausencia_de_Item = df_motivo.contagem_filtramotivo('Item presente na Nota Fiscal mas não foi entregue')
 
-#////////////////////////////////////////////////////////////////
-def QuantidadeEntregue():
-  engine = get_connection()
-  list_entregue = []
-  with engine.connect() as conn:
-        query_entregue  = (text(f"""
-        select COUNT(lg.CodigoPedido) as Quantidade_Entregue from HauszMapa.Pedidos.LogPedidos lg
-        JOIN (SELECT CodigoPedido
-        ,MAX(IdLog) ultimaetapa
-        FROM [HauszMapa].[Pedidos].[LogPedidos]
-        WHERE IdUsuarioAlteracao = 'Aplicacao' 
-        GROUP BY CodigoPedido) maxlog
-        ON maxlog.CodigoPedido = lg.CodigoPedido
-        where ParaIdEtapaFlexy = 9 
-		    AND lg.IdLog = maxlog.ultimaetapa
-        AND DataAtualizacao between '{mespassado}-01' and '{mes_atual}-01'
-		    AND IdUsuarioAlteracao = 'Aplicacao'
-        """))
-        lista_entreguetotal = conn.execute(query_entregue).all()
-        for lista in lista_entreguetotal:
-            dict_filtro_hoje = {}
-            for keys, values in lista.items():
-                dict_filtro_hoje[keys] = values
-            list_entregue.append(dict_filtro_hoje)
-
-  return list_entregue
-
-def QuantidadeAvarias():
-  engine = get_connection()
-  list_avaria = []
-  with engine.connect() as conn:
-        query_avaria = (text(f"""
-        select count(avarias.Avarias) QuantidadeAvaria from
-        (select  DISTINCT lr.CodigoPedido Avarias
-		from HauszMapa.Logistica.LogReversaOcorrencia lr
-		join(select CodigoPedido, max(IdOcorrencia) ultimaetapa ,max(DataInserido) ultimadata
-		from HauszMapa.Logistica.LogReversaOcorrencia 
-		where IdCausaOcorrencia = 1
-		GROUP BY CodigoPedido) maxlog on maxlog.CodigoPedido = lr.CodigoPedido
-		--where lr.IdCausaOcorrencia = 1
-		and DataInserido between '{mespassado}-01' and '{mes_atual}-01') avarias
-        """))
-        lista_avaria = conn.execute(query_avaria).all()
-        for lista in lista_avaria:
-            dict_avaria = {}
-            for keys, values in lista.items():
-                dict_avaria[keys] = values
-            list_avaria.append(dict_avaria)
-
-  return list_avaria
-
+#Essas funções tem uma base de consulta no MYSQL, e estamos acessando os valores totais retornados delas.  
 def Total_Avaria():
   Avarias = QuantidadeAvarias()
   Total_de_Avarias = Avarias[0]['QuantidadeAvaria']
--- Loc/queries/query_avarias.sql
+++ Loc/queries/query_avarias.sql
@@ -10,3 +10,56 @@ select count(avarias.Avarias) QuantidadeAvaria from
 
 
 		--funcionalidade month(getdate()) filtrar meses apartir de uma data
+
+
+/////////
+
+def QuantidadeEntregue():
+  engine = get_connection()
+  list_entregue = []
+  with engine.connect() as conn:
+        query_entregue  = (text(f"""
+        select COUNT(lg.CodigoPedido) as Quantidade_Entregue from HauszMapa.Pedidos.LogPedidos lg
+        JOIN (SELECT CodigoPedido
+        ,MAX(IdLog) ultimaetapa
+        FROM [HauszMapa].[Pedidos].[LogPedidos]
+        WHERE IdUsuarioAlteracao = 'Aplicacao' 
+        GROUP BY CodigoPedido) maxlog
+        ON maxlog.CodigoPedido = lg.CodigoPedido
+        where ParaIdEtapaFlexy = 9 
+		    AND lg.IdLog = maxlog.ultimaetapa
+        AND DataAtualizacao between '{mespassado}-01' and '{mes_atual}-01'
+		    AND IdUsuarioAlteracao = 'Aplicacao'
+        """))
+        lista_entreguetotal = conn.execute(query_entregue).all()
+        for lista in lista_entreguetotal:
+            dict_filtro_hoje = {}
+            for keys, values in lista.items():
+                dict_filtro_hoje[keys] = values
+            list_entregue.append(dict_filtro_hoje)
+
+  return list_entregue
+
+def QuantidadeAvarias():
+  engine = get_connection()
+  list_avaria = []
+  with engine.connect() as conn:
+        query_avaria = (text(f"""
+        select count(avarias.Avarias) QuantidadeAvaria from
+        (select  DISTINCT lr.CodigoPedido Avarias
+		from HauszMapa.Logistica.LogReversaOcorrencia lr
+		join(select CodigoPedido, max(IdOcorrencia) ultimaetapa ,max(DataInserido) ultimadata
+		from HauszMapa.Logistica.LogReversaOcorrencia 
+		where IdCausaOcorrencia = 1
+		GROUP BY CodigoPedido) maxlog on maxlog.CodigoPedido = lr.CodigoPedido
+		--where lr.IdCausaOcorrencia = 1
+		and DataInserido between '{mespassado}-01' and '{mes_atual}-01') avarias
+        """))
+        lista_avaria = conn.execute(query_avaria).all()
+        for lista in lista_avaria:
+            dict_avaria = {}
+            for keys, values in lista.items():
+                dict_avaria[keys] = values
+            list_avaria.append(dict_avaria)
+
+  return list_avaria
